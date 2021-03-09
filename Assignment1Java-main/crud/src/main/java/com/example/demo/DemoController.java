02f4992d1559d98a06f0d11404d1e51d0c662173
Tallner Christian <tallnerc@br-automation.com>
2021-03-09 15:58:21 +0100

    First REST for Post and Put implemented

--- Assignment1Java-main/crud/src/main/java/com/example/demo/DemoController.java
+++ Assignment1Java-main/crud/src/main/java/com/example/demo/DemoController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.*;
 
 
@@ -39,6 +42,32 @@ public class DemoController {
     @GetMapping(path="/player/{id}")
     Player getSingle(@PathVariable Integer id){
         return playerRepository.findById(id).get();
+    }
+
+    @PutMapping(path="/player/{id}", consumes="application/json", produces="application/json")
+    Player update(@PathVariable Integer id, @RequestBody Player updatedPlayer){
+        Player dbPlayer = playerRepository.findById(id).get();
+
+        dbPlayer.setBorn(updatedPlayer.getBorn());
+        dbPlayer.setAge(updatedPlayer.getAge());
+        dbPlayer.setName(updatedPlayer.getName());
+        dbPlayer.setJersey(updatedPlayer.getJersey());
+
+        playerRepository.save(dbPlayer);
+
+        return dbPlayer;
+    }
+
+    @PostMapping(path="/player", consumes="application/json", produces="application/json")
+    ResponseEntity<Object> add(@RequestBody Player p){
+    //void add(@RequestBody Player p){
+        playerRepository.save(p);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(p.getId())
+        .toUri();
+        return ResponseEntity.created(location).build();
     }
     
 /** /
